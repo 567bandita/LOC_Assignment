6fb4ee72ea1aa56270d6cf3b9455708b583ea6c6
567bandita <99343596+567bandita@users.noreply.github.com>
2022-02-12 22:20:30 +0530

    Add files via upload

---
+++ Ans2.c
@@ -0,0 +1,12 @@
+#include<math.h>
+#include<stdio.h>
+void main () 
+{
+int x, y;
+printf("Enter two no. \n") ;
+scanf("%d%d",&x,&y);
+if(x>y) 
+printf ("x is greater");
+else;
+printf("b is greater") ;
+}
