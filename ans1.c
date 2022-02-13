ed864f96dd4647ca9b43d529f92e9987fe90074a
567bandita <99343596+567bandita@users.noreply.github.com>
2022-02-13 09:39:33 +0530

    Add files via upload

---
+++ ans1.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+
+int main()
+{
+    //prgrm to display basic information
+    char a[100],c[100],d[100];
+    int b;
+    printf("Name:Bandita Behera\n");
+    scanf("%s",&a);
+    printf("Registration no.:2102050081 \n");
+    scanf("%d",&b);
+    printf("Branch:Electrical engineering \n");
+    scanf("%s",&c);
+    printf("Hobbies:listening music and reading fiction novel\n");
+    scanf("%s",&d);
+    return 0;
+}
