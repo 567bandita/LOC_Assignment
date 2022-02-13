734e913cbae4858f94efbee7f4e98a72c8007e83
567bandita <99343596+567bandita@users.noreply.github.com>
2022-02-13 09:59:04 +0530

    Delete Ans1.c

--- Ans1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() 
-{
-printf("Name:Bandita Behera\n");
-printf("Regd.no:2102050081\n");
-printf("Branch:Electrical engineering\n");
-printf("Hobbies:listening music,reading fiction novel");
-getchar() ;
-}
